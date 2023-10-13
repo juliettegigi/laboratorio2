81d1c3526eb4c2efaf8c7059df54761313dfa4d1
Marina Julieta Gutierrez <111161094+juliettegigi@users.noreply.github.com>
2023-10-13 16:52:23 -0300

    Update login.js

--- controllers/login.js
+++ controllers/login.js
@@ -1,6 +1,7 @@
 const{response}=require('express');
 const Sequelize = require('sequelize');
 const bcryptjs=require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const {Usuario,Rol}=require("../models")
 const { generarJWT } = require('./funciones/jwt');
@@ -17,9 +18,6 @@ const login=async(req,res=response)=>{
     const{email,contrasena,nombreRol}=req.body;
        //verificar si el email existe
        try{
-   console.log("hola");
-   console.log({email});
-   console.log({contrasena});
            const usuario = await Usuario.findOne({
                where: {  [Sequelize.Op.or]: [ { email }, { documento: email } ] },
                include: [
@@ -29,15 +27,17 @@ const login=async(req,res=response)=>{
                  }
                ]
              });
-             console.log(!usuario);
+
          if(!usuario)return res.render("index",{email:"Usuario o rol incorrecto.",pass:"",passValue:contrasena,emailValue:email})
          
         const passValida=await bcryptjs.compare(contrasena,usuario.contrasena);
-        console.log({passValida});
         if(!passValida) return res.render("index",{email:"",pass:"Contraseña incorrecta.",passValue:contrasena,emailValue:email});   
        
         const token=await generarJWT(usuario.id);
-        return res.render("index",{token,passValue:contrasena,emailValue:email})
+       // const token = jwt.sign({ id:usuario.id }, process.env.SECRETORPRIVATEKEY);
+       //res.header('Authorization', token);
+       //res.redirect(`/pacientes`);
+       res.redirect(`/pacientes?token=${token}`);
        }
        catch(error){
         console.log(error);
@@ -48,4 +48,5 @@ const login=async(req,res=response)=>{
    
 
 module.exports={login} 
+
 
