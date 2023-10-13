591e90151b218b5dc99ef620ccc410213fcf5fc2
Marina Julieta Gutierrez <111161094+juliettegigi@users.noreply.github.com>
2023-10-13 16:47:57 -0300

    Update index.js

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,31 +1,11 @@
-(async () => {
-  document.addEventListener("DOMContentLoaded", async () => {
-    if (token) {
-      sessionStorage.setItem('token', token);
+  formu.addEventListener("click", (e) => {
+    const campoEnfocado = e.target;
 
-      try {
-        const rta=await fetch("http://localhost:3000/users/inicio", {
-          headers: { "token": token },
-          redirect: "follow"
-        });
-        if( rta.redirected ){
-          window.location=rta.url;//redirecciono
-        }
-      } catch (err) {
-        console.log(err);
-      }
+    if (campoEnfocado.id === "usuario") {
+      const p = document.getElementById("errorEmail");
+      if (p) p.remove();
+    } else if (campoEnfocado.id === "contrasena") {
+      const p = document.getElementById("errorPass");
+      if (p) p.remove();
     }
-
-    formu.addEventListener("click", (e) => {
-      const campoEnfocado = e.target;
-
-      if (campoEnfocado.id === "usuario") {
-        const p = document.getElementById("errorEmail");
-        if (p) p.remove();
-      } else if (campoEnfocado.id === "contrasena") {
-        const p = document.getElementById("errorPass");
-        if (p) p.remove();
-      }
-    });
   });
-})();
