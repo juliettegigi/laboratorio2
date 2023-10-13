a77b1b0f06cb63635b647c147e6c86bc8b344038
Marina Julieta Gutierrez <111161094+juliettegigi@users.noreply.github.com>
2023-10-13 16:56:09 -0300

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,7 @@
 const bcryptjs=require('bcryptjs');
-const { Sequelize,Op} = require('sequelize');
+const { Sequelize} = require('sequelize');
 
-const {Usuario,Rol,UsuarioRol}=require('../models');
+const {Usuario,Rol}=require('../models');
 
    
 
@@ -42,62 +42,11 @@ const userPost = async (req, res) => {
 
 
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-const buscarPacientes = async( termino = '', res = response ) => {
-try
-{
-    // término puede ser: documento , apellido o email 
-    //IE, me pueden buscar por documento, apellido, email
-
-    
-    const regex = new RegExp( termino, 'i' );
-    console.log({regex});
-    // se forma una expresion regular con el termino, 
-    //para que me busque todo lo que contenga al "termino"  ej: termino=fer ==> me buscaria "fernanda","fernando","fernandez",etc
-    //'i' es insensible a si está en mayus o no 
-    const pacientes = await Usuario.findAll({
-        include: [ { model: Rol, through: UsuarioRol, where: { nombre: 'Paciente' }}],
-        where: { [Op.or]: [ { documento: { [Op.regexp]: termino } },
-                            { email: { [Op.regexp]: termino } },
-                            { apellido: { [Op.regexp]: termino } }]
-               }
-      });
-
-    res.json({
-        cantidad:pacientes.length,
-        pacientes
-    });
-}
-catch(err){
-    console.log(err);
-    res.json({
-        err
-    });
-}
-}
-
-
-
-///////////////////////////////////////////
-
-const buscar=(req,res=response)=>{
-
-//localhost:3000/buscar/documento||email||apellido
-console.log("req.params,termino= ",req.params.termino);
-   const{termino}=req.params;
-   buscarPacientes(termino, res);
-  
-}
-
-
 
 
 
 module.exports={
-   userPost,buscar
+   userPost
 }
 
 
@@ -117,5 +66,3 @@ module.exports={
 
 
 */
-
-
