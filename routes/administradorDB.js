3472b7af5dfddb6e3bdcb1f7cf6e651f5ae1f941
Santiago <santigonzalez555@gmail.com>
2023-10-18 18:51:47 -0300

    orden + ruta propia

--- routes/administradorDB.js
+++ routes/administradorDB.js
@@ -1,10 +1,8 @@
 const{Router}=require('express');
 const { userPost}=require('../controllers/users');
-const {ordenlogin}=require('../controllers/orden');
 
 const router=Router();
 
-router.post('/',ordenlogin);
 router.post('/',userPost);
 
 module.exports=router;
