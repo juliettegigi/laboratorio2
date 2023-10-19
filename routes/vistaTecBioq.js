213dec062a390a2026c8d98f319dc7a17cf7f561
juliettegigi <mar9ina@gmail.com>
2023-10-19 18:34:43 -0300

    j11

--- routes/vistaTecBioq.js
+++ routes/vistaTecBioq.js
@@ -1,6 +1,7 @@
 const{Router}=require('express');
 const router=Router();
 
-router.get('/inicio',(req,res)=>{res.render("tecnicoBioq/inicio")})
+//router.get('/inicio',(req,res)=>{res.render("tecnicoBioq/inicio")})
+router.get('/inicio',(req,res)=>{res.render("inicioAdmin2/inicioAdmin2")})
 router.get('/formDeterminacion',(req,res)=>{res.render("tecnicoBioq/formDeterminacion")})
 module.exports=router
